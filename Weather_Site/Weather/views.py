37bb28f0ccdef6224cf1bf18b89fcafa02149915
svetl <v.svetlichniy1999@gmail.com>
2022-12-06 17:25:16 +0300

    Added information about weather on page 'index.html'

--- Weather_Site/Weather/views.py
+++ Weather_Site/Weather/views.py
@@ -4,8 +4,22 @@ import requests
 
 def index(request):
     appid = "c285d41d09ba722c41714e9e75753558"
-    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid=" + appid
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
 
-    city = 'London'
+    city = 'Minsk'
+    res = requests.get(url.format(city)).json()
 
-    return render(request, 'Weather/index.html')
+    print(res)
+
+    city_info = {
+        'city': city,
+        'temp': res['main']['temp'],
+        'humidity': res['main']['humidity'],
+        'feels': res['main']['feels_like'],
+        'wind_speed': res['wind']['speed'],
+        'icon': res['weather'][0]['icon']
+    }
+
+    context = {'info': city_info}
+
+    return render(request, 'Weather/index.html', context)
