15be2b548bcf98dcd5d73abdb64016e847ba8abd
svetl <v.svetlichniy1999@gmail.com>
2022-12-06 10:42:42 +0300

    Created page 'base.html' and 'index.html'

--- Weather_Site/Weather/views.py
+++ Weather_Site/Weather/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+
+def index(request):
+    appid = "c285d41d09ba722c41714e9e75753558"
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid=" + appid
+
+    city = 'London'
+
+    return render(request, 'Weather/index.html')
